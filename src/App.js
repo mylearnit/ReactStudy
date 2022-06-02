b3d2921b99108083cc8c26a224854bf20599d13c
suhailvs <suhailvs@gmail.com>
2022-06-02 10:01:33 +0530

    added add employee and edit employee

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { EmployeeList } from './components/EmployeeList';
+import { Route, Switch } from 'react-router-dom';
 
 import { GlobalProvider } from './context/GlobalState';
+
+import { Home } from './components/Home';
+import { AddEmployee } from './components/AddEmployee';
+import { EditEmployee } from './components/EditEmployee';
 
 function App() {
   return (
     <GlobalProvider>
       <div className="App">
-        <EmployeeList />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/add" component={AddEmployee} exact />
+          <Route path="/edit/:id" component={EditEmployee} exact />
+        </Switch>
       </div>
     </GlobalProvider>
   );
